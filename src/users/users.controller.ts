d215897ee58f262cd8259b3cdd21c49b4e888be8
minhlip <minhlips2002@gmail.com>
2024-08-11 17:05:10 +0700

    feat: user schema

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() request: CreateUserRequest) {
+    await this.userService.create(request);
+  }
+}
